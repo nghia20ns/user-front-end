2c7f713443805627d614a85d8e50c17870445a7b
nghia20ns <quocnghia91ll@.com>
2023-07-31 16:05:01 +0700

    update global state

--- src/Components/ProductComponent/AddProduct.js
+++ src/Components/ProductComponent/AddProduct.js
@@ -1,67 +1,79 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import '../css/product.css'; // Import CSS file
-import Alert from '../Alert';
+import React, { useContext, useState } from "react";
+import axios from "axios";
+import "../css/product.css"; // Import CSS file
+import Alert from "../Alert";
+import { Context } from "../../Store/Store";
+import { actions } from "../../Store/Index";
 
 const AddProduct = () => {
-  const [apiInput, setApiInput] = useState('');
-  const [messageError, setMessageError] = useState("");
-  const [isShowAlert, setIsShowAlert] = useState(false);
+  const [apiInput, setApiInput] = useState("");
+  const [state, dispatch] = useContext(Context);
 
   const createProduct = async () => {
     try {
       const dataObject = JSON.parse(apiInput);
       if (Array.isArray(dataObject)) {
-        await axios.post('http://45.77.215.103/api/product/create', {
-          apiInput: dataObject,
-        })
-        .then((res) => {
-          if (res.data.status === "success") {
-            setIsShowAlert(true);
-            setMessageError(res.data.message);
-            setApiInput('')
-          } else if (res.data.status === "error") {
-            setIsShowAlert(true);
-            setMessageError(res.data.message);
-          }
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
+        await axios
+          .post("http://45.77.215.103/api/product/create", {
+            apiInput: dataObject,
+          })
+          .then((res) => {
+            if (res.data.status === "success") {
+              dispatch(actions.isAlert(true));
+              dispatch(actions.showMessageAlert(res.data.message));
+              setApiInput("");
+            } else if (res.data.status === "error") {
+              dispatch(actions.isAlert(true));
+              dispatch(actions.showMessageAlert(res.data.message));
+            }
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
       } else {
-        console.log('Dữ liệu không hợp lệ! Chuỗi JSON phải là một mảng đối tượng.');
+        console.log(
+          "Dữ liệu không hợp lệ! Chuỗi JSON phải là một mảng đối tượng."
+        );
       }
     } catch (error) {
-      console.log('Lỗi:', error);
+      console.log(error);
+      dispatch(actions.isAlert(true));
+      dispatch(actions.showMessageAlert("error"));
     }
+  };
+  const deleteAlert = () => {
+    dispatch(actions.isAlert(false));
+    dispatch(actions.showMessageAlert(""));
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     createProduct();
+    setTimeout(deleteAlert, 5000);
   };
 
   return (
-<>   
-{isShowAlert && <Alert message={messageError} />}
- <div className="form-container">
-      <form onSubmit={handleSubmit} className="form-wrapper">
-        <h2 className="form-title">ADD</h2>
-        <textarea
-          rows={15}
-          type="text"
-          value={apiInput}
-          onChange={(e) => setApiInput(e.target.value)}
-          className="form-input"
-          placeholder="Nhập chuỗi API..."
-        />
-        <button type="submit" className="form-button">
-          Thêm dữ liệu
-        </button>
-      </form>
-    </div>
+    <>
+      <div className="row" style={{ height: 50 }}>
+        {state.isAlert && <Alert message={state.showMessageAlert} />}
+      </div>{" "}
+      <div className="form-container">
+        <form onSubmit={handleSubmit} className="form-wrapper">
+          <h2 className="form-title">ADD</h2>
+          <textarea
+            rows={15}
+            type="text"
+            value={apiInput}
+            onChange={(e) => setApiInput(e.target.value)}
+            className="form-input"
+            placeholder="Nhập chuỗi API..."
+          />
+          <button type="submit" className="form-button">
+            Thêm dữ liệu
+          </button>
+        </form>
+      </div>
     </>
-
   );
 };
 
